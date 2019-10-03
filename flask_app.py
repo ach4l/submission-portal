83e3210a95068a6e77ebcf51619396a6d3ef5399
ach4l <achal@cmi.ac.in>
2019-10-03 09:19:03 +0000

    beautified but accuracy still not working

--- flask_app.py
+++ flask_app.py
@@ -1,48 +1,73 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request
-from processing import do_calculation
+from flask import Flask, make_response, request, render_template
+import io
+import csv
+#import pandas as pd
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-@app.route("/", methods=["GET", "POST"])
-def adder_page():
-    errors = ""
-    if request.method == "POST":
-        number1 = None
-        number2 = None
-        try:
-            number1 = float(request.form["number1"])
-        except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number1"])
-        try:
-            number2 = float(request.form["number2"])
-        except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number2"])
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
 
-        if number1 is not None and number2 is not None:
-            result = do_calculation(number1, number2)
-            return '''
-                <html>
-                    <body>
-                        <p>The result is {result}</p>
-                        <p><a href="/">Click here to calculate again</a>
-                    </body>
-                </html>
-            '''.format(result=result)
-    return '''
+
+@app.route('/')
+def form():
+    return render_template("main_page.html")
+
+@app.route('/transform', methods=["GET","POST"])
+def transform_view():
+
+    roll_no = request.form["roll_no"]
+    print(roll_no)
+
+    f = request.files['data_file']
+
+    if not f:
+        return "No file"
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    #print("file contents: ", file_contents)
+    #print(type(file_contents))
+
+    correct_csv = csv.reader("test.csv")
+
+
+    print(csv_input)
+
+    error_count = 0
+    total_count = 0
+
+    for line in csv_input:
+        total_count = total_count + 1
+        if line not in correct_csv:
+            error_count = error_count + 1
+
+    accuracy = error_count / total_count * 100
+
+    with open('results_raw.csv','a') as fd:
+        print("Writing :" + roll_no+"," + str(accuracy))
+        fd.write(roll_no+"," + str(accuracy))
+
+
+
+    #stream.seek(0)
+    #result = transform(stream.read())
+
+    #response = make_response(result)
+    #response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return """
         <html>
             <body>
-                {errors}
-                <p>Enter your numbers:</p>
-                <form method="post" action=".">
-                    <p><input name="number1" /></p>
-                    <p><input name="number2" /></p>
-                    <p><input type="submit" value="Do calculation" /></p>
-                </form>
+                <h1>Accuracy is</h1> {accuracy}
             </body>
         </html>
-    '''.format(errors=errors)
+    """.format(accuracy = accuracy)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
 
